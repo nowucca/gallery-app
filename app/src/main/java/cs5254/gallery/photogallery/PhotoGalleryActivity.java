9db90b056634ddd2b0eafea109a7416ecdb22ccd
Steven Atkinson <steven@atkinson.net>
2018-04-03 00:08:17 -0700

    Initial version of the app based on BNR Chapter 25

---
+++ app/src/main/java/cs5254/gallery/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,11 @@
+package cs5254.gallery.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
